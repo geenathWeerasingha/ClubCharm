d53397f9071847ac8944ee519297ad5381bc165d
geenathWeerasingha <73776728+geenathWeerasingha@users.noreply.github.com>
2023-12-22 12:16:28 +0530

    first commit

---
+++ src/main/java/com/rungroup/web/service/ClubService.java
@@ -0,0 +1,24 @@
+package com.rungroup.web.service;
+
+import com.rungroup.web.dto.ClubDto;
+import com.rungroup.web.models.Club;
+
+import java.util.List;
+
+public interface ClubService {
+    List<ClubDto> findAllClubs();
+
+    Club saveClub(ClubDto clubDto);
+
+    ClubDto findClubById(long clubId);
+
+
+    void updateClub(ClubDto club);
+
+    void delete(Long clubId);
+
+    List<ClubDto> searchClubs(String query);
+
+
+
+}
---
+++ src/main/java/com/rungroup/web/service/EventService.java
@@ -0,0 +1,17 @@
+package com.rungroup.web.service;
+
+import com.rungroup.web.dto.EventDto;
+
+import java.util.List;
+
+public interface EventService {
+    void createEvent(Long clubId, EventDto eventDto);
+
+    List<EventDto> findAllEvents();
+
+    EventDto findEventById(Long eventId);
+
+    void updateEvent(EventDto eventDto);
+
+    void deleteEvent(long eventId);
+}
